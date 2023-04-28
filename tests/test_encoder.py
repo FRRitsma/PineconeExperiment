00a7f8124e90238c2619251a0f8a96c16196109b
Folkert Ritsma <f.r.ritsma@gmail.com>
2023-04-28 14:44:15 +0200

    Struggling with pytest

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,5 +1,17 @@
 # %%
+from embedding.encode_img import embed_image
+from pathlib import Path
+import os
+
+current_dir = Path.cwd()
+parent_dir = current_dir.parent
+os.chdir(parent_dir)
 
 
 def test_always_passes():
+    assert True
+
+
+def test_embed_iamge():
+    embed_image()
     assert True
