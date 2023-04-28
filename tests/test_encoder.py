14b98f1e79ba8868875a8c853ac2453133357930
Folkert Ritsma <f.r.ritsma@gmail.com>
2023-04-28 22:59:20 +0200

    Adding new tests

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -3,13 +3,15 @@
 from embedding.encode_img import embed_image, load_image_from_file
 import os
 
+# This is needed to load files in the directory correctly:
+here = os.path.dirname(os.path.abspath(__file__))
+
 
 def test_always_passes():
     assert True
 
 
 def test_embed_image():
-    here = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(here, "input.jpg")
     img = load_image_from_file(filename)
     embed_image(img)
