aa420f609f698bb8a2738b55b6e17bd72c40ae47
Folkert Ritsma <f.r.ritsma@gmail.com>
2023-04-30 21:54:47 +0200

    Executed similarity search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # %%
 import pinecone
-
-# import numpy as np
 from settings import API_KEY, ENVIRONMENT, INDEX_NAME, DIMENSION
 
 
@@ -10,12 +8,9 @@ pinecone.init(api_key=API_KEY, environment=ENVIRONMENT)
 
 # Add index if it does not already exist:
 if INDEX_NAME not in pinecone.list_indexes():
-
     # Add index name to pinecone:
-    pinecone.create_index(INDEX_NAME, DIMENSION)
+    pinecone.create_index(INDEX_NAME, DIMENSION, metric="euclidean")
 
-
-# %%
 index = pinecone.Index(index_name=INDEX_NAME)
 
 index.upsert(
@@ -28,23 +23,8 @@ index.upsert(
     ]
 )
 
-# # Define the target vector for similarity search
-# target_vector = np.array([0.4, 0.3, 0.5, 0.2, 0.1], dtype=np.float16)
+# Perform the similarity search
+query_vector = [0.155] * DIMENSION
+results = index.query(queries=[query_vector], top_k=10)
 
-# # Insert the target vector into the index
-# doc_id = "my_doc_id"
-# index.upsert(ids=[doc_id], vectors=[target_vector])
-
-# %%
-
-# # Perform the similarity search
-# query_vector = target_vector
-# results = index.query(queries=[query_vector], top_k=10)
-
-# # Print the results
-# print("Similarity search results:")
-# for result in results[0]:
-#     print(result.id, result.score)
-
-# # Close the connection to Pinecone
 pinecone.deinit()
