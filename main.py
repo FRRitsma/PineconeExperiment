7f5a7f45ab5f8f25d3a93cf28dd682a483b8e409
Folkert Ritsma <f.r.ritsma@gmail.com>
2023-04-30 21:30:36 +0200

    Succesful pinecone connection achieved

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 # %%
 import pinecone
 
+# import numpy as np
+from settings import API_KEY, ENVIRONMENT, INDEX_NAME, DIMENSION
+
+
 # Connect to Pinecone
-pinecone.init(api_key="YOUR_API_KEY")
-index = pinecone.Index(index_name="my_index", mode="w")
+pinecone.init(api_key=API_KEY, environment=ENVIRONMENT)
 
-# Define the target vector for similarity search
-target_vector = [0.4, 0.3, 0.5, 0.2, 0.1]
+# Add index if it does not already exist:
+if INDEX_NAME not in pinecone.list_indexes():
 
-# Insert the target vector into the index
-doc_id = "my_doc_id"
-index.upsert(ids=[doc_id], vectors=[target_vector])
+    # Add index name to pinecone:
+    pinecone.create_index(INDEX_NAME, DIMENSION)
 
-# Perform the similarity search
-query_vector = target_vector
-results = index.query(queries=[query_vector], top_k=10)
 
-# Print the results
-print("Similarity search results:")
-for result in results[0]:
-    print(result.id, result.score)
+# %%
+index = pinecone.Index(index_name=INDEX_NAME)
 
-# Close the connection to Pinecone
+index.upsert(
+    [
+        ("A", [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
+        ("B", [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
+        ("C", [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]),
+        ("D", [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
+        ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]),
+    ]
+)
+
+# # Define the target vector for similarity search
+# target_vector = np.array([0.4, 0.3, 0.5, 0.2, 0.1], dtype=np.float16)
+
+# # Insert the target vector into the index
+# doc_id = "my_doc_id"
+# index.upsert(ids=[doc_id], vectors=[target_vector])
+
+# %%
+
+# # Perform the similarity search
+# query_vector = target_vector
+# results = index.query(queries=[query_vector], top_k=10)
+
+# # Print the results
+# print("Similarity search results:")
+# for result in results[0]:
+#     print(result.id, result.score)
+
+# # Close the connection to Pinecone
 pinecone.deinit()
---
+++ settings.py
@@ -0,0 +1,4 @@
+API_KEY: str = "ae0bb7c3-fa52-4b41-9ce2-fdb0e701f080"
+ENVIRONMENT: str = "us-west4-gcp"
+INDEX_NAME: str = "first-index"
+DIMENSION: int = 8
