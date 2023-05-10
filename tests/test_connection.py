b07909e054ef6e1a1d87daa22600c697fe9aa6f1
FRRitsma <folkert.ritsma@xomnia.com>
2023-05-10 22:32:13 +0200

    Added functional test for api key

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,7 +1,15 @@
 import pinecone
+import pytest
+from pinecone import UnauthorizedException
 
 from settings import API_KEY
 from settings import ENVIRONMENT
+
+
+def test_incorrect_api_key():
+    pinecone.init(api_key="NONSENSE", environment=ENVIRONMENT)
+    with pytest.raises(UnauthorizedException):
+        pinecone.create_index("NONSENSE", dimension=8, metric="euclidean")
 
 
 def test_connection():
