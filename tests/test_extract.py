b55de8400a2b5d471a4437a802d847f5b0e4fd7b
FRRitsma <folkert.ritsma@xomnia.com>
2023-05-10 18:42:28 +0200

    Added 2 more tests for test_extract

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -2,6 +2,10 @@ from src.extract.extract import extract_images
 from src.extract.extract import SetPath
 
 
+def test_difference_between_train_and_val_path():
+    assert SetPath.train != SetPath.val
+
+
 def test_train_images_extraction():
     n_labels: int = 5
     n_images: int = 7
@@ -20,3 +24,10 @@ def test_val_images_extraction():
     assert isinstance(val_images, list)
     first_element = val_images[0]
     assert isinstance(first_element, dict)
+
+
+def test_extract_more_labels_than_exist():
+    n_labels: int = 20
+    n_images: int = 7
+    train_images = extract_images(n_labels, n_images, SetPath.train)
+    assert len(train_images) == 10 * n_images
